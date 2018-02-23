7cd332bac82bc396b7e2d4a86e75b43d33781fd8
vikneshkumar <36755547+vikneshjohn@users.noreply.github.com>
2018-02-23 15:57:32 +0530

    Create vicky3.py

---
+++ vicky3.py
@@ -0,0 +1,7 @@
+a = input("Enter a character: ")
+
+if(a=='A' or a=='a' or a=='E' or a =='e' or a=='I'
+ or a=='i' or any=='O' or a=='o' or a=='U' or a=='u'):
+    print(a, "is a Vowel")
+else:
+    print(a, "is a Consonant")
